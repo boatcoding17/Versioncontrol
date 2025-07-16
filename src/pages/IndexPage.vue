f2028ca98253a9dbfbcf7c68bec1db425db1175f
boatcoding17 <boatsamantachai@gmail.com>
2025-07-16 11:56:49 +0700

    edit label 123

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,7 +9,7 @@
       <q-input
         filled
         v-model="name"
-        label="ชื่อ-สกุล"
+        label="ชื่อ-สกุล123"
         hint="กรุณาใส่ชื่อและนามสกุล"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'กรุณาใส่ข้อความที่ถูกต้อง']"
