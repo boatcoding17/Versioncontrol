c7b390db1f1fd15b57b339a04ac68d595475f06e
boatcoding17 <boatsamantachai@gmail.com>
2025-08-27 20:38:04 +0700

    hello quasar!

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,12 +4,13 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    />
+
+    <h1>Hello Quasar!</h1>
+    <p>
+      This is a starter project for Quasar Framework v2.
+      <br />
+      To get started, edit <code>src/pages/IndexPage.vue</code>.
   </q-page>
 </template>
 
