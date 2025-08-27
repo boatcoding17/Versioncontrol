adafffc411668a5f466cf4ae7aeb7a5278df379a
boatcoding17 <boatsamantachai@gmail.com>
2025-08-27 20:34:06 +0700

    indexpage welcome and name

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,18 +1,18 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center column">
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    
+    
+    <p>Welcome to Your Quasar App</p>
+    <p>My name is Samantachai Naksuk 6704101374</p>
   </q-page>
 </template>
 
 <script setup>
-//
+// logic เขียนตรงนี้
 </script>
+
